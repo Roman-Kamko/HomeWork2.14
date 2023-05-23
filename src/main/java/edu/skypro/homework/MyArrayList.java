3adab7514aa8ca3f8dad813c42887123f2adf86a
RomanKamko <r.kamko@mail.ru>
2023-05-23 15:03:37 +0400

    - done

--- src/main/java/edu/skypro/homework/MyArrayList.java
+++ src/main/java/edu/skypro/homework/MyArrayList.java
@@ -1,14 +1,15 @@
 package edu.skypro.homework;
 
-import edu.skypro.homework.exceptions.StorageIsFullException;
 import edu.skypro.homework.exceptions.BadIndexException;
+import edu.skypro.homework.exceptions.ElementNotFoundException;
+import edu.skypro.homework.exceptions.StorageIsFullException;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class MyArrayList {
     private String[] arr;
     private final int DEFAULT_CAPACITY = 3;
-    private int initialCapacity;
     private int size = 0;
 
     public MyArrayList(int initialCapacity) {
@@ -24,11 +25,7 @@ public class MyArrayList {
     }
 
     public String add(String string) {
-        if (string == null) {
-            throw new IllegalArgumentException(
-                    "Хранилище не может содержать " + string
-            );
-        }
+        checkElement(string);
 
         if (size < arr.length) {
             for (int i = 0; i < arr.length; i++) {
@@ -46,24 +43,106 @@ public class MyArrayList {
     }
 
     public String add(int index, String string) {
-        if (index < 0) {
-            throw new BadIndexException(
-                    "Индекс не может быть отрицательным"
-            );
-        }
-
-        if (index < arr.length) {
-            arr[index] = string;
-        } else {
-            throw new BadIndexException(
-                    "указан несуществующий индекс " + index
-            );
-        }
-
+        checkElement(string);
+        checkIndex(index);
+        arr[index] = string;
         return string;
     }
 
-    public String[] getAll() {
+    public String set(int index, String string) {
+        add(index, string);
+        return string;
+    }
+
+    public String remove(String string) {
+        checkElement(string);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null && arr[i].equals(string)) {
+                arr[i] = null;
+                return string;
+            }
+        }
+        throw new ElementNotFoundException(
+                "Элемент не найден " + string
+        );
+    }
+
+    public String remove(int index) {
+        checkIndex(index);
+        String removedElement = arr[index];
+        arr[index] = null;
+        return removedElement;
+    }
+
+    public boolean contains(String string) {
+        checkElement(string);
+        for (String s : arr) {
+            if (s != null && s.equals(string)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(String string) {
+        checkElement(string);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null && arr[i].equals(string)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(String string) {
+        checkElement(string);
+        for (int length = arr.length - 1; length > 0; length--) {
+            if (arr[length] != null && arr[length].equals(string)) {
+                return length;
+            }
+        }
+        return -1;
+    }
+
+    public String get(int index) {
+        checkIndex(index);
+        return arr[index];
+    }
+
+    public boolean equals(MyArrayList myArrayList) {
+        if (myArrayList == null) {
+            throw new IllegalArgumentException("Для сравнения передан null");
+        }
+        if (this == myArrayList) {
+            return true;
+        }
+        if (getClass() != myArrayList.getClass()) {
+            return false;
+        }
+        String[] otherArr = myArrayList.toArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null && !arr[i].equals(otherArr[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isEmpty() {
+        int counter = 0;
+        for (String s : arr) {
+            if (s != null) {
+                counter++;
+            }
+        }
+        return counter == 0;
+    }
+
+    public void clear() {
+        Arrays.fill(arr, null);
+    }
+
+    public String[] toArray() {
         String[] newArr = new String[size];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != null) {
@@ -73,8 +152,44 @@ public class MyArrayList {
         return newArr;
     }
 
+    private void checkIndex(int index) {
+        if (index < 0) {
+            throw new BadIndexException(
+                    "Индекс не может быть отрицательным"
+            );
+        }
+        if (index >= arr.length) {
+            throw new BadIndexException(
+                    "указан не существующий индекс " + index
+            );
+        }
+    }
+
+    private void checkElement(String string) {
+        if (string == null) {
+            throw new IllegalArgumentException(
+                    "Хранилище не может содержать null"
+            );
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyArrayList that = (MyArrayList) o;
+        return size == that.size && Arrays.equals(arr, that.arr);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size);
+        result = 31 * result + Arrays.hashCode(arr);
+        return result;
+    }
+
     @Override
     public String toString() {
-        return Arrays.toString(getAll());
+        return Arrays.toString(toArray());
     }
 }
--- src/test/java/edu/skypro/homework/MyArrayListTest.java
+++ src/test/java/edu/skypro/homework/MyArrayListTest.java
@@ -1,19 +1,18 @@
 package edu.skypro.homework;
 
 import edu.skypro.homework.exceptions.BadIndexException;
+import edu.skypro.homework.exceptions.ElementNotFoundException;
 import edu.skypro.homework.exceptions.StorageIsFullException;
-import net.bytebuddy.asm.MemberSubstitution;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.context.annotation.PropertySource;
 
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class MyArrayListTest {
     private MyArrayList out;
@@ -54,6 +53,7 @@ class MyArrayListTest {
         String[] expected = {"privet", "bye"};
         assertThat(out.add(1, "bye")).isIn(expected);
     }
+
     public static Stream<Arguments> provideParamsForBadIndexExceptionTest() {
         return Stream.of(
                 Arguments.of(-2, "hi"),
@@ -66,5 +66,97 @@ class MyArrayListTest {
     void addWithIndexNegativeTest(int index, String string) {
         assertThatExceptionOfType(BadIndexException.class)
                 .isThrownBy(() -> out.add(index, string));
+    }
+
+    @Test
+    void removeByElementTest() {
+        String[] expected = {"privet"};
+        assertThat(out.remove("poka"))
+                .isEqualTo("poka")
+                .isNotIn(expected);
+    }
+
+    @Test
+    void removeByElementNegativeTest() {
+        assertThatExceptionOfType(ElementNotFoundException.class)
+                .isThrownBy(() -> out.remove("ok"));
+    }
+
+    @Test
+    void removeByIndexTest() {
+        String[] expected = {"privet"};
+        assertThat(out.remove(1))
+                .isEqualTo("poka")
+                .isNotIn(expected);
+    }
+
+    @Test
+    void sizeTest() {
+        assertThat(out.size())
+                .isEqualTo(2);
+    }
+
+    @Test
+    void containsTest() {
+        assertThat(out.contains("poka"))
+                .isTrue();
+    }
+
+    @Test
+    void containsNegativeTest() {
+        assertThat(out.contains("ok"))
+                .isFalse();
+    }
+
+    @Test
+    void indexOfTest() {
+        assertThat(out.indexOf("poka"))
+                .isEqualTo(1);
+    }
+
+    @Test
+    void lastIndexOfTest() {
+        assertThat(out.lastIndexOf("poka"))
+                .isEqualTo(1);
+    }
+
+    @Test
+    void getTest() {
+        assertThat(out.get(1))
+                .isEqualTo("poka");
+    }
+
+    @Test
+    void equalsTest() {
+        MyArrayList test1 = new MyArrayList(5);
+        test1.add("privet");
+        test1.add("poka");
+
+        MyArrayList test2 = new MyArrayList(5);
+        test2.add("privet");
+        test2.add("ok");
+
+        assertThat(out.equals(test1)).isTrue();
+        assertThat(out.equals(test2)).isFalse();
+        assertThat(out.equals(out.toArray())).isFalse();
+    }
+
+    @Test
+    void equalsNegativeTest() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> out.equals(null));
+    }
+
+    @Test
+    void isEmptyTest() {
+        MyArrayList testList = new MyArrayList();
+        assertThat(out.isEmpty()).isFalse();
+        assertThat(testList.isEmpty()).isTrue();
+    }
+
+    @Test
+    void clearTest() {
+        out.clear();
+        assertThat(out.isEmpty()).isTrue();
     }
 }
